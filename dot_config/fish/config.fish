3764ffb028e8eae4c5c187bab740da61e60526f3
Dennis <1451140+karaktaka@users.noreply.github.com>
2026-02-21 21:46:39 +0100

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,6 +6,9 @@ if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
     source /usr/share/cachyos-fish-config/cachyos-config.fish
 end
 
+# Local NPM Packages
+set -gx PATH $HOME/.npm-global/bin $PATH
+
 # Key bindings
 bind alt-right nextd-or-forward-word
 bind alt-left prevd-or-backward-word
